4f809dbc6664fbf71563a19d0e3f36e53153f1ff
Samarthbhat52 <126409331+Samarthbhat52@users.noreply.github.com>
2024-04-30 13:34:02 +0530

    feat: Simplify the home page layout (#8)

--- src/app/(authenticated)/home/page.tsx
+++ src/app/(authenticated)/home/page.tsx
@@ -1,9 +1,7 @@
 const Home = () => {
   return (
     <div className="flex flex-1 flex-col items-center justify-center">
-      <h1>Hello world Page</h1>
-      <p>Testing conflicts</p>
-      <p>another</p>
+      <h1>Home Page</h1>
     </div>
   );
 };
